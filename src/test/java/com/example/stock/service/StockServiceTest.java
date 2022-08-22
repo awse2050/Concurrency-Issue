a0062c11b7f78ab7d64459e844f1a7459c4a3295
yoonhwanSS <wesaq11@gmail.com>
2022-08-22 23:36:25 +0900

    [#3] 동시성 문제 발생의 시작

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -44,4 +48,38 @@ class StockServiceTest {
 
         assertEquals(99, stock.getQuantity());
     }
+
+    /*
+      여러개의 요청을 한번에 보내줘야 하기 때문에 멀티 쓰레드를 이용해야 한다.
+      ExecuteService - 비동기로 실행하는 작업을 단순화하여 할수있게 도와주는 java api
+
+      100개의 요청이 기다려야 하므로 CountdownLatch 를 사용
+      CountdownLatch -> 다른 쓰레드에서 수행중인 작업이 완료될때까지 대기할 수 있도록 도와주는 클래스이다.
+   */
+    @Test
+    void 동시에_100개_요청() throws InterruptedException {
+        // 쓰레드 개수
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i< threadCount; i++) {
+            executorService.submit(() -> {
+                // 한 쓰레드 당 한 개의 재고를 떨어뜨린다.
+                try {
+                    stockService.decrease(1L ,1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        //모든 요청 완료시 값을 조회 후 비교
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertEquals(0L, stock.getQuantity());
+    }
+
 }
