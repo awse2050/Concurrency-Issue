aa53db5267ddd623f6eae7a68f34b6820d89d4b3
yoonhwanSS <wesaq11@gmail.com>
2022-08-26 00:29:20 +0900

    [#5] Pessimistic Lock 을 사용한 동시성 해결

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class StockServiceTest {
 
     @Autowired
-    private StockService stockService;
+    private PessimisticLockService stockService;
 
     @Autowired
     private StockRepository stockRepository;
